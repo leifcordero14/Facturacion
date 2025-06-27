37eb323829904209e3eddd965feb5494c2cfb985
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-06-27 12:10:48 -0400

    Add article to DB context

---
+++ Facturacion/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Facturacion.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Facturacion.Data
+{
+  public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+  {
+    public DbSet<Article> Article { get; set; }
+
+  }
+}
