ec8fef60f5ea32431cf797f05f76c34fe3b1bad6
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-06-27 18:18:08 -0400

    Add creation and update validator for article entity

---
+++ Facturacion/Validators/CreateArticleValidator.cs
@@ -0,0 +1,26 @@
+﻿using Facturacion.DTOs;
+using FluentValidation;
+
+namespace Facturacion.Validators
+{
+  public class CreateArticleValidator : AbstractValidator<CreateArticleDto>
+  {
+    public CreateArticleValidator()
+    {
+      RuleFor(a => a.Description)
+        .NotEmpty()
+        .WithMessage("La descripción no puede estar vacía")
+        .MaximumLength(100)
+        .WithMessage("La descripción no puede exceder los 100 caracteres");
+
+      RuleFor(a => a.UnitPrice)
+        .GreaterThanOrEqualTo(0)
+        .WithMessage("El precio unitario no puede ser un número negativo");
+
+      RuleFor(a => a.IsAvailable)
+        .Must(v => v == true || v == false)
+        .WithMessage("Valor inválido para disponibilidad")
+        .When(a => a.IsAvailable.HasValue);
+    }
+  }
+}
