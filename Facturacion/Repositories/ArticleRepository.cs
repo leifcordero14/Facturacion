14acbb9dd40b1713033b4e8ba0b1bdbdde9218c4
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-06-27 12:12:09 -0400

    Add article repository

---
+++ Facturacion/Repositories/ArticleRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Facturacion.Repositories
+{
+  public class ArticleRepository
+  {
+  }
+}
---
+++ Facturacion/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Facturacion.Repositories
+{
+  public interface IRepository<TEntity>
+  {
+    Task<IEnumerable<TEntity>> GetAll();
+    Task<TEntity> GetById(int id);
+    Task Create(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    Task Save();
+  }
+}
