f689ae5b1e01b66e69475f87f14406bbb1025f77
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-06-27 16:03:49 -0400

    Add article DTOs for creation and update

---
+++ Facturacion/DTOs/CreateArticleDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Facturacion.DTOs
+{
+  public class CreateArticleDto
+  {
+    public required string Description { get; set; }
+    public decimal UnitPrice { get; set; }
+    public bool? IsAvailable { get; set; }
+  }
+}
---
+++ Facturacion/DTOs/UpdateArticleDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Facturacion.DTOs
+{
+  public class UpdateArticleDto
+  {
+    public string? Description { get; set; }
+    public decimal? UnitPrice { get; set; }
+    public bool? IsAvailable { get; set; }
+  }
+}
