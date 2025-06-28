47e0f1f91c627814fb13f56bb91f2aeadbdb2927
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-06-28 15:41:14 -0400

    Implement seller repository

---
+++ Facturacion/Repositories/SellerRepository.cs
@@ -0,0 +1,40 @@
+﻿using Facturacion.Data;
+using Facturacion.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Facturacion.Repositories
+{
+  public class SellerRepository(ApplicationDbContext context) : IRepository<Seller>
+  {
+    private readonly ApplicationDbContext _context = context;
+
+    public async Task<IEnumerable<Seller>> GetAll()
+    {
+      return await _context.Seller.ToListAsync();
+    }
+    public async Task<Seller?> GetById(int id)
+    {
+      return await _context.Seller.FindAsync(id);
+    }
+    public async Task Create(Seller seller)
+    {
+      await _context.Seller.AddAsync(seller);
+      return;
+    }
+    public void Update(Seller seller)
+    {
+      _context.Seller.Update(seller);
+      return;
+    }
+    public void Delete(Seller seller)
+    {
+      _context.Seller.Remove(seller);
+      return;
+    }
+    public async Task Save()
+    {
+      await _context.SaveChangesAsync();
+      return;
+    }
+  }
+}
