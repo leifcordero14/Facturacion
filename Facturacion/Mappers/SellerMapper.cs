d53363b9d17b858c526b6ed4e804c86b95bfa5b2
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-06-28 16:14:33 -0400

    Add seller mapper

---
+++ Facturacion/Mappers/SellerMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Facturacion.DTOs;
+using Facturacion.Models;
+
+namespace Facturacion.Mappers
+{
+  public class SellerMapper : Profile
+  {
+    public SellerMapper()
+    {
+      CreateMap<Seller, SellerDto>();
+
+      CreateMap<CreateSellerDto, Seller>()
+        .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive ?? true))
+        .ForMember(dest => dest.CommissionPercentage, opt => opt.MapFrom(src => src.CommissionPercentage ?? 0));
+    }
+  }
+}
