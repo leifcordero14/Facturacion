7703404cdd94c84a6e382659db81c4921bff4130
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-06-28 19:14:10 -0400

    Add creation and update validators for seller entity

---
+++ Facturacion/Validators/CreateSellerValidator.cs
@@ -0,0 +1,29 @@
+﻿using Facturacion.DTOs;
+using FluentValidation;
+
+namespace Facturacion.Validators
+{
+  public class CreateSellerValidator : AbstractValidator<CreateSellerDto>
+  {
+    public CreateSellerValidator()
+    {
+      RuleFor(s => s.FirstName)
+        .NotEmpty()
+        .WithMessage("El nombre es obligatorio");
+
+      RuleFor(s => s.LastName)
+        .NotEmpty()
+        .WithMessage("El apellido es obligatorio");
+
+      RuleFor(s => s.CommissionPercentage)
+        .InclusiveBetween((byte)0, (byte)100)
+        .WithMessage("El porcentaje de comisión debe estar entre 0 y 100")
+        .When(s => s.CommissionPercentage.HasValue);
+
+      RuleFor(s => s.IsActive)
+        .Equal(s => s.IsActive == true || false)
+        .WithMessage("Valor inválido para disponibilidad")
+        .When(s => s.IsActive.HasValue);
+    }
+  }
+}
---
+++ Facturacion/Validators/UpdateSellerValidator.cs
@@ -0,0 +1,29 @@
+﻿using Facturacion.DTOs;
+using FluentValidation;
+
+namespace Facturacion.Validators
+{
+  public class UpdateSellerValidator : AbstractValidator<UpdateSellerDto>
+  {
+    public UpdateSellerValidator()
+    {
+      RuleFor(s => s.FirstName)
+        .NotEmpty()
+        .WithMessage("El nombre es obligatorio");
+
+      RuleFor(s => s.LastName)
+        .NotEmpty()
+        .WithMessage("El apellido es obligatorio");
+
+      RuleFor(s => s.CommissionPercentage)
+        .InclusiveBetween((byte)0, (byte)100)
+        .WithMessage("El porcentaje de comisión debe estar entre 0 y 100")
+        .When(s => s.CommissionPercentage.HasValue);
+      
+      RuleFor(a => a.IsActive)
+        .Must(v => v == true || v == false)
+        .WithMessage("Valor inválido para disponibilidad")
+        .When(a => a.IsActive.HasValue);
+    }
+  }
+}
