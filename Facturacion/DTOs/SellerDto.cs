b6ab6622b7fb4aaad628ed75db802cf2da2535b5
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-06-28 15:53:37 -0400

    Add DTOs for seller model, seller creation and seller update

---
+++ Facturacion/DTOs/SellerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Facturacion.DTOs
+{
+  public class SellerDto
+  {
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public byte CommissionPercentage { get; set; }
+    public bool IsActive { get; set; }
+  }
+}
---
+++ Facturacion/DTOs/UpdateSellerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Facturacion.DTOs
+{
+  public class UpdateSellerDto
+  {
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public byte? CommissionPercentage { get; set; }
+    public bool? IsActive { get; set; }
+  }
+}
