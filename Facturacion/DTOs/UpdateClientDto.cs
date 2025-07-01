64d9925e87942930693a2f615f606ac44face56f
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-07-01 13:44:44 -0400

    Add DTOs for client model, client creation and client update

---
+++ Facturacion/DTOs/UpdateClientDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Facturacion.DTOs
+{
+  public class UpdateClientDto
+  {
+    public required string BussinesName { get; set; }
+    public required string IdentificationNumber { get; set; }
+    public required string LedgerAccount { get; set; }
+    public bool IsActive { get; set; }
+  }
+}
