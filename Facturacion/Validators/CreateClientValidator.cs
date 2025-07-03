e7cf10282a6c4a95057567a3b73c4331ccad81ea
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-07-03 14:48:58 -0400

    Add validators for client creation and update

---
+++ Facturacion/Validators/CreateClientValidator.cs
@@ -0,0 +1,28 @@
+﻿using Facturacion.DTOs;
+using Facturacion.Utilities;
+using FluentValidation;
+
+namespace Facturacion.Validators
+{
+  public class CreateClientValidator : AbstractValidator<CreateClientDto>
+  {
+    public CreateClientValidator()
+    {
+      RuleFor(c => c.BusinessName)
+        .NotEmpty()
+        .WithMessage("El nombre comercial o razón social es obligatorio")
+        .MaximumLength(100)
+        .WithMessage("El nombre comercial o razón social no puede exceder los 100 caracteres");
+
+      RuleFor(c => c.IdentificationNumber)
+        .NotEmpty()
+        .WithMessage("El RNC o cédula es obligatorio")
+        .Must(IdentificationNumberHelper.IsValidCedulaOrRNC)
+        .WithMessage("El RNC o cédula es inválido");
+
+      RuleFor(c => c.LedgerAccount)
+        .NotEmpty()
+        .WithMessage("La cuenta contable es obligatoria");
+    }
+  }
+}
