52498f44a7cf1d5815ab51100c51ea7b323ea2a9
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-07-23 17:32:08 -0400

    Implement filter functionality for billing controller

--- Facturacion/Controllers/BillingsController.cs
+++ Facturacion/Controllers/BillingsController.cs
@@ -10,10 +10,12 @@ namespace Facturacion.Controllers
   [ApiController]
   public class BillingsController(
     ICreateReadService<BillingDto, CreateBillingDto> billingService,
+    IFilterService<BillingDto, BillingFilterDto> billingFilterService,
     IValidationResultHelper validationResultHelper,
     IValidator<CreateBillingDto> createValidator) : ControllerBase
   {
     private readonly ICreateReadService<BillingDto, CreateBillingDto> _billingService = billingService;
+    private readonly IFilterService<BillingDto, BillingFilterDto> _billingFilterService = billingFilterService;
     private readonly IValidationResultHelper _validationResultHelper = validationResultHelper;
     private readonly IValidator<CreateBillingDto> _createValidator = createValidator;
 
@@ -21,6 +23,26 @@ namespace Facturacion.Controllers
     public async Task<ActionResult<IEnumerable<BillingDto>>> GetAll()
     {
       var billingDtos = await _billingService.GetAll();
+      return Ok(billingDtos);
+    }
+
+    [HttpGet("search")]
+    public async Task<ActionResult<IEnumerable<BillingDto>>> GetByFilter(
+      [FromQuery] int? articleId,
+      [FromQuery] int? sellerId,
+      [FromQuery] int? clientId,
+      [FromQuery] DateTime? startDate,
+      [FromQuery] DateTime? endDate)
+    {
+      var filter = new BillingFilterDto
+      {
+        ArticleId = articleId,
+        SellerId = sellerId,
+        ClientId = clientId,
+        StartDate = startDate,
+        EndDate = endDate
+      };
+      var billingDtos = await _billingFilterService.GetByFilter(filter);
       return Ok(billingDtos);
     }
 
