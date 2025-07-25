386e3e525e8438db8e474104be85e8699dda5071
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-07-25 14:58:56 -0400

    Implement user repository

---
+++ Facturacion/Repositories/AuthRepository.cs
@@ -0,0 +1,24 @@
+﻿using Facturacion.Data;
+using Facturacion.Models;
+
+namespace Facturacion.Repositories
+{
+  public class AuthRepository(ApplicationDbContext context) : IAuthRepository
+  {
+    private readonly ApplicationDbContext _context = context;
+    public async Task<User?> GetByEmail(string email)
+    {
+      return await _context.User.FindAsync(email);
+    }
+    public async Task Create(User user)
+    {
+      await _context.User.AddAsync(user);
+      return;
+    }
+    public async Task Save()
+    {
+      await _context.SaveChangesAsync();
+      return;
+    }
+  }
+}
---
+++ Facturacion/Repositories/IAuthRepository.cs
@@ -0,0 +1,11 @@
+﻿using Facturacion.Models;
+
+namespace Facturacion.Repositories
+{
+  public interface IAuthRepository
+  {
+    Task<User?> GetByEmail(string email);
+    Task Create(User user);
+    Task Save();
+  }
+}
