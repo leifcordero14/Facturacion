b8cbbae7258727d1da19af50973bc1409b86830f
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-07-25 14:53:45 -0400

    Implement PasswordHashManager class

---
+++ Facturacion/Utilities/PasswordHashManager.cs
@@ -0,0 +1,24 @@
+﻿using Facturacion.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Facturacion.Utilities
+{
+  public class PasswordHashManager
+  {
+    private readonly PasswordHasher<User> _passwordHasher = new();
+    public string HashPassword(User user, string password)
+    {
+      var hashedPassword = _passwordHasher.HashPassword(user, password);
+      return hashedPassword;
+    }
+    public PasswordVerificationResult VerifyHashedPassword(User user, string hashedPassword, string password)
+    {
+      var result = _passwordHasher.VerifyHashedPassword(user, hashedPassword, password);
+      return result;
+    }
+    public bool IsValidPassword(User user, string hashedPassword, string password)
+    {
+      return VerifyHashedPassword(user, hashedPassword, password) != PasswordVerificationResult.Failed;
+    }
+  }
+}
